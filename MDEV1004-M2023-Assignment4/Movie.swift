c37a47291dd903c3455c70198813da575912b91b
Namrata Savaliya <namratasavaliya@Namratas-MacBook-Air.local>
2023-08-17 16:48:39 -0400

    ios code for assignment4

---
+++ MDEV1004-M2023-Assignment4/Movie.swift
@@ -0,0 +1,17 @@
+struct Movie: Codable
+{
+    let _id: String
+    let movieID: String
+    let title: String
+    let studio: String
+    let genres: [String]
+    let directors: [String]
+    let writers: [String]
+    let actors: [String]
+    let year: Int
+    let length: Int
+    let shortDescription: String
+    let mpaRating: String
+    let posterLink: String
+    let criticsRating: Double
+}
---
+++ MDEV1004-M2023-Assignment4/MovieTableViewCell.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class MovieTableViewCell: UITableViewCell 
+{
+    
+    @IBOutlet weak var titleLabel: UITextField!
+    
+    @IBOutlet weak var studioLabel: UITextField!
+    
+    @IBOutlet weak var ratingLabel: UITextField!
+}
